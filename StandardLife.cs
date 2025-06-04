0bac17c43256163ded98a6e7757505b2d64d9a4f
rubenyanez123 <144442667+rubenyanez123@users.noreply.github.com>
2025-06-04 11:25:19 +0200

    Add files via upload
    This is the file for Week 4 - assignment 2, Strategy Pattern (ConwayGameOfLife-student)

---
+++ StandardLife.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConwayGameOfLife
+{
+    internal class StandardLife : ConwayGameOfLife
+    {
+        public override bool CellShouldLive(bool livingCell, int neighbourCount)
+        {
+            // B3/S23
+
+            // cell is born if it has 3 neighbors
+            if (!livingCell && (neighbourCount == 3))
+                return true;
+
+            // cell survives if it has 2 or 3 neighbors
+            if (livingCell && (neighbourCount == 2 || neighbourCount == 3))
+                return true;
+
+            // dead cell
+            return false;
+        }
+    }
+}
