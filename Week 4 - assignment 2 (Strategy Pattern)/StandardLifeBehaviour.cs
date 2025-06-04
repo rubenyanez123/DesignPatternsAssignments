509b156192a14087e162c616d2ed60fe65868179
rubenyanez123 <144442667+rubenyanez123@users.noreply.github.com>
2025-06-04 11:39:07 +0200

    Add files via upload
    I wasn't able to add the entire solution since it was too big, so I added these without the build files

---
+++ Week 4 - assignment 2 (Strategy Pattern)/StandardLifeBehaviour.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConwayGameOfLife
+{
+    internal class StandardLifeBehaviour : ILifeBehaviour
+    {
+        public bool CellShouldLive(bool livingCell, int neighbourCount)
+        {
+            // B3/S23
+
+            // a cell is born if it has 3 neighbors
+            if (!livingCell && (neighbourCount == 3))
+                return true;
+
+            // a cell survives if it has 2 or 3 neighbors
+            if (livingCell && (neighbourCount == 2 || neighbourCount == 3))
+                return true;
+
+            // dead cell
+            return false;
+        }
+    }
+}
